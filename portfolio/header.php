4a25c6523a67bba0044322507dc73eda0fa2ec57
ThomasBjornstad <justsendto@me.com>
2019-06-21 13:50:53 +0200

    Fjernet ting

--- portfolio/header.php
+++ portfolio/header.php
@@ -16,7 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-
+	<script src="<?php echo get_template_directory_uri(); ?>/js/custom.js" charset="utf-8"></script>
 	<?php wp_head(); ?>
 </head>
 
